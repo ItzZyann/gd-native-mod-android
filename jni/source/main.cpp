c710671d2a298957735d1c256883030f48067575
ItzZyann <satoukazuma270@gmail.com>
2026-04-17 18:04:05 +0800

    Update for v2.2081 Android

--- jni/source/main.cpp
+++ jni/source/main.cpp
@@ -1,29 +1,81 @@
 #include "incl.h"
-#include "MenuLayer.h"
 
+// Simple example mod
+// Simple example mod
+// Simple example mod
+
+// we're gonna hook the main menu (MenuLayer)
+// and just add a "Hello World" text in the middle
+
+// pointer to the original function
 bool (*MenuLayer_initO)(MenuLayer*);
-bool MenuLayer_initH(MenuLayer* pThis) {
-    if(!MenuLayer_initO(pThis)) return false;
 
-    auto dir = CCDirector::sharedDirector();
-    auto winSize = dir->getWinSize();
+// our custom version of init
+bool MenuLayer_initH(MenuLayer* self) {
 
+    // always call the original first
+    if(!MenuLayer_initO(self)) 
+        return false;
+
+    // get screen size
+    auto director = CCDirector::sharedDirector();
+    auto winSize = director->getWinSize();
+
+    // make a text label
     auto label = CCLabelBMFont::create("Hello World", "bigFont.fnt");
-    label->setScale(.8);
+
+    // make it a bit smaller and center it
+    label->setScale(0.8f);
     label->setPosition(winSize / 2);
-    pThis->addChild(label, 100);
+
+    // add it to the menu
+    self->addChild(label, 100);
 
     return true;
 }
 
+
+// ==============================
+// replacing a button
+// ==============================
+
+// this one completely replaces the original function
+// no need to call the original here
+
 void MenuLayer_onMoreGames() {
-    // lets try the static hook
+
+    // when "More Games" is clicked, show a popup instead
+    FLAlertLayer::create(
+        nullptr,
+        "Alert",
+        "This is an alert.",
+        "OK",
+        nullptr,
+        330
+    )->show();
 }
 
-__attribute__((constructor))
-void lib_gdmod() {
 
-    hook("_ZN9MenuLayer4initEv", MenuLayer_initH, MenuLayer_initO);
-    _hook("", MenuLayer_onMoreGames);
-    
+// ==============================
+// entry point (runs when mod loads)
+// ==============================
+
+[[gnu::constructor]]
+void mod_init() {
+
+    // you usually get these names from tools like
+    // ghidra or ida (or just from Addr.txt if you have it)
+
+    // hook the menu init
+    hook(
+        "_ZN9MenuLayer4initEv",
+        MenuLayer_initH,
+        MenuLayer_initO
+    );
+
+    // hook the "More Games" button
+    _hook(
+        "_ZN9MenuLayer11onMoreGamesEPN7cocos2d8CCObjectE",
+        MenuLayer_onMoreGames
+    );
 }
